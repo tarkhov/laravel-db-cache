24dd5ef96d5b7090f39a15e2cb481350ce1af2f8
tarkhov <agtuks@gmail.com>
2026-02-01 15:23:37 +0200

    cacheKey fix

--- src/CacheKey.php
+++ src/CacheKey.php
@@ -8,11 +8,10 @@ class CacheKey
 {
     public static function make(Builder | EloquentBuilder $builder): string
     {
-        $query = $builder->getQuery();
-        $sql = str_replace('?', '%s', $query->toSql());
+        $sql = str_replace('?', '%s', $builder->toSql());
         $bindings = array_map(function ($binding) {
             return is_numeric($binding) ? $binding : "'" . $binding . "'";
-        }, $query->getBindings());
+        }, $builder->getBindings());
         return sha1(vsprintf($sql, $bindings));
     }
 }
